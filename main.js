52986f575d7c3d102cf2d67f6534ddf97af4d3ac
Mariola <maariolaa@gmail.com>
2024-11-14 11:52:29 +0100

    BemAndDOM

--- main.js
+++ main.js
@@ -5,3 +5,8 @@ console.log(age);
 
 let ishappy='false';
 console.log(false);
+
+const header = document.querySelector('h1')
+console.log(header)
+console.log(header.style)
+header.style.color='red';
