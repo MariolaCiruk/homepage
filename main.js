249c9803ff8200755b5f6695ee8736005949e9b9
Mariola <maariolaa@gmail.com>
2024-11-14 14:53:53 +0100

    pusty element zmieniony dzieki js

--- main.js
+++ main.js
@@ -6,7 +6,11 @@ console.log(age);
 let ishappy='false';
 console.log(false);
 
+
 const header = document.querySelector('h1')
 console.log(header)
 console.log(header.style)
 header.style.color='red';
+
+const section = document.querySelector('.page__header--js')
+section.innerHTML = `Nazywam się ${name} i mam ${age} laaata. czas na zmiany:D`
