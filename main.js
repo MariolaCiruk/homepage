571490cd67a4ce0ada5261288d4f1de15b3daddc
Mariola <maariolaa@gmail.com>
2025-01-16 13:19:28 +0100

    flex_obiektyJS

--- main.js
+++ main.js
@@ -23,3 +23,34 @@ conJS.innerHTML = "dlaczego tu dziala a tam nie"
 
 const conJSA = document.querySelector(`.abouthtml__content--jsas`)
 conJSA.innerHTML ="BAKSIO BAKSIO BAKSIO"
+
+const greet = (name, age) => {
+    const result = `Witaj ${name} Masz ${age} lat! `;
+    return result;
+}
+const greeting = greet (`Mariola`, 32);
+console.log(greeting);
+console.log(greet(`Karolina`,34));
+
+const deathStar = {
+    diameter: 120000,
+    fire: (target) => {
+        console.log(`${target} destroyed `)
+    },
+    isOperating: true,
+    levels:357,
+    name: `Death Star`,
+    population: 1000,
+    commander: {
+        name:`darth vader`
+    }
+}
+
+deathStar.fire(`alderaan`)
+deathStar.fire(`Mileeeenaaaaa`)
+
+console.log(deathStar[`levels`])
+console.log(deathStar.commander.name)
+
+
+
